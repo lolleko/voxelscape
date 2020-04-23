4dbd947a2bff22a069497261d156a3a3e7569142
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-04-23 14:25:39 +0200

    First triangle
    http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/

---
+++ include/vs_shader_loader.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "glad/glad.h"
+
+GLuint VSLoadShaders(const char* vertex_file_path, const char* fragment_file_path);
--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "vs_shader_loader.h"
 #include "vs_ui.h"
 #include "vs_ui_state.h"
 
@@ -62,6 +63,37 @@ int main(int, char**)
 
     const auto* uiState = UI.getState();
 
+    // START http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,
+        -1.0f,
+        0.0f,
+        1.0f,
+        -1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+    };
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(
+        GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = VSLoadShaders(
+        "shaders/vertex/SimpleVertexShader.glsl", "shaders/fragment/SimpleFragmentShader.glsl");
+    // END Tutorial 2
+
     // Main loop
     while (glfwWindowShouldClose(window) == 0)
     {
@@ -76,6 +108,26 @@ int main(int, char**)
         const auto clearColor = uiState->clearColor;
         glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // START http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+        glUseProgram(programID);
+
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+            3,  // size
+            GL_FLOAT,  // type
+            GL_FALSE,  // normalized?
+            0,         // stride
+            (void*)0   // array buffer offset
+        );
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3);  // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
+        // END Tutorial 2
+
         UI.draw();
         glfwSwapBuffers(window);
     }
