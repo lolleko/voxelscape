c41167d05c81e2cc9a6aff08f3e23c212d06adf7
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-05-10 17:33:17 +0200

    Readded unifroms for model drawing

--- source/renderer/vs_model.cpp
+++ source/renderer/vs_model.cpp
@@ -1,4 +1,6 @@
 #include "renderer/vs_model.h"
+#include "world/vs_world.h"
+#include "core/vs_camera.h"
 
 VSModel::VSModel(std::string const& path, bool gamma)
     : gammaCorrection(gamma)
@@ -12,6 +14,14 @@ VSModel::~VSModel() {
 
 void VSModel::draw(VSWorld* world, std::shared_ptr<VSShader> shader) const
 {
+    const auto model = glm::scale(glm::mat4(1.f), {3.f, 3.f, 3.f});
+    shader->uniforms()
+        .setVec3("lightPos", world->getDirectLightPos())
+        .setVec3("lightColor", world->getDirectLightColor())
+        .setVec3("viewPos", world->getCamera()->getPosition())
+        .setMat4("model", model)
+        .setMat4("MVP", world->getCamera()->getMVPMatrixFast(model));
+
     for (const auto& mesh : meshes)
     {
         mesh.draw(world, shader);
