d32186eb1045d8cc409f1fdba55bf4925a7bd0d8
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-05-10 15:52:21 +0200

    Add references for mesh/model loading

--- include/renderer/vs_mesh.h
+++ include/renderer/vs_mesh.h
@@ -11,6 +11,8 @@
 #include "renderer/vs_vertex_context.h"
 #include "renderer/vs_drawable.h"
 
+// Roughly based on https://learnopengl.com/Model-Loading/Mesh
+
 struct VSTexture
 {
     unsigned int id;
