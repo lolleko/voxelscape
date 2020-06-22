179fbd0e573d56f0fc1c669779e261e4cbf8be8a
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-06-22 12:05:14 +0200

    Use world context instead of singleton

--- include/game/systems/hover_system.h
+++ include/game/systems/hover_system.h
@@ -7,17 +7,20 @@
 #include "game/components/inputs.h"
 #include "game/components/hoverable.h"
 #include "game/components/bounds.h"
+#include "game/components/world_context.h"
 #include "world/vs_world.h"
 
 void updateHoverSystem(entt::registry& registry)
 {
     auto& inputs = registry.ctx<Inputs>();
 
+    const auto& worldContext = registry.ctx<WorldContext>();
+
     inputs.hoverEntity = entt::null;
 
     if (inputs.mouseTrace.bHasHit)
     {
-        registry.view<Hoverable, Location, Bounds>().each([&inputs](
+        registry.view<Hoverable, Location, Bounds>().each([&inputs, &worldContext](
                                                               auto entity,
                                                               const Hoverable& hoverable,
                                                               const Location& location,
@@ -29,7 +32,7 @@ void updateHoverSystem(entt::registry& registry)
             {
                 inputs.hoverEntity = entity;
                 // Add visual indicator
-                VSApp::getInstance()->getWorld()->getDebugDraw()->drawBox(
+                worldContext.world->getDebugDraw()->drawBox(
                     {location + bounds.min, location + bounds.max}, hoverable.hoverColor);
                 // For now stop on the first intersection
                 return;
